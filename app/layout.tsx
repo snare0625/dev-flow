b3d2e2646bb49eb711ed6e5c5c619965886d94bd
Neo Sekhukhune <sekhukhune056@gmail.com>
2025-02-16 13:36:37 +0200

    Updated Public file

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of Stack Overflow.",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
 };
 
 export default function RootLayout({
